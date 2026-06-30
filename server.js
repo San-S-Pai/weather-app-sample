6e274431592a2ebf97cdb5691736c83dc2aa9874
San-S-Pai <ssanchithpai@outlook.com>
2026-06-30 15:07:41 +0530

    add initial ability to call weather for cities by search

--- server.js
+++ server.js
@@ -2,8 +2,33 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send('<h1>Hello, Express.js Server!</h1>');
+    res.send('<h1>Welcome to Weather App Sample Server!</h1>');
+    res.send('<h2>Available API calls:</h2>');
+    res.send('<h3>/cities - gets weather for cities</h3>');
 });
+
+app.get('/cities', (req, res) => {
+    async function getWeather() {
+        try {
+            //Step 1: Get the city
+            const city = await fetch("https://geocoding-api.open-meteo.com/v1/search?name=Berlin&count=10&language=en&format=json");
+
+            //Step 2: Check the response
+            if (!city.ok) {
+                throw new fetchError(`Fetching data failed. Code: ${city.status}`)
+            }
+
+            //Step 3: Extract JSON
+            const data = await city.json();
+            console.log(data);
+            res.json(data);
+        } catch (fetchError) { //Step 4: Log errors encountered
+            console.log(fetchError);
+        }
+    }
+
+    getWeather();
+})
 
 const port = process.env.PORT || 3000; // You can use environment variables 
 app.listen(port, () => {
